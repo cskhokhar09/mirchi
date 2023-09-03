cba207ca83efa3203024d586e1baa159cc177a39
Cy Khokhar <cskhokhar09@gmail.com>
2023-09-03 04:19:35 -0700

    custom button in progress work

--- app/src/main/java/com/csk/mirchi/MainActivity.kt
+++ app/src/main/java/com/csk/mirchi/MainActivity.kt
@@ -3,94 +3,61 @@ package com.csk.mirchi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.csk.mirchi.ui.presentation.GroceryViewModel
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.csk.mirchi.ui.theme.MirchiTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private val viewModel by viewModels<GroceryViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MirchiTheme {
-                QuadrantScreen()
-            }
-        }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CustomButton(title = "Click Me", color = "0xFFEFB8C8", textColor = "0xFF7D5260", modifier = Modifier.size(200.dp, 20.dp))
+                }
 
-        val flow : Flow<Int> = flowOf(0)
-
-        CoroutineScope(Dispatchers.Main).launch {
-            flow.collect{
-                println(it)
             }
         }
 
     }
 }
 
+private fun parseColor(color: String) : Color{
+    return Color(android.graphics.Color.parseColor(color))
+}
+
 @Composable
-fun QuadrantScreen(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .weight(0.5f)) {
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .weight(0.5f)
-                .background(Color.Yellow)) {
-
-            }
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .weight(0.5f)
-                .background(Color.Blue)){
-
-            }
-
-        }
-
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .weight(0.5f)
-            .background(Color.Cyan)) {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .weight(0.5f)
-                .background(Color.Cyan)) {
-
-            }
-
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .weight(0.5f)
-                .background(Color.Green)){
-
-            }
-        }
+fun CustomButton(title: String, color: String, textColor: String, modifier: Modifier = Modifier) {
+    var title1 by remember { mutableStateOf(title) }
+    Button(onClick = {
+                    title1 = "Done"
+    }, colors = ButtonDefaults.buttonColors(containerColor = Color.White), border = BorderStroke(2.dp, Color.Yellow), modifier = modifier) {
+        Text(text = title1, fontSize = 14.sp, color = Color.Green)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun QuadrantScreenPreview() {
-    MirchiTheme {
-        QuadrantScreen()
+fun CustomButtonPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        CustomButton(title = "Click Me", color = "0xFFEFB8C8", textColor = "0xFF7D5260", modifier = Modifier.wrapContentSize().fillMaxWidth())
     }
 }
+
